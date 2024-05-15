41385467c03dd1f6ae929fc9c3da0533a3a2d86b
Edwin574 <owinoedwin574@gmail.com>
2024-05-16 01:11:53 +0300

    initial commit

--- meningitis/interventions.py
+++ meningitis/interventions.py
@@ -16,7 +16,34 @@ class Vaccine(ss.Intervention):
             to_vaccinate = self.prob > np.random.rand(n_eligible)
             vaccine_ids = eligible_ids[to_vaccinate]
             meningitis.immunity[vaccine_ids] += self.imm_boost
+            
+# Define people with the right age structure
+class VaccineAge(ss.Intervention):
+    def __init__(self, age_range=None, timestep=10, prob=0.5, imm_boost=2.0):
+        super().__init__() # Initialize the intervention
+        self.timestep = timestep # Store the timestep the vaccine is applied on
+        self.prob = prob # Store the probability of vaccination
+        self.imm_boost = imm_boost # Store the amount by which immunity is boosted
+        self.age_range = age_range
 
+    def apply(self, sim): # Apply the vaccine
+        if sim.ti == self.timestep: # Only apply on the matching timestep
+            meningitis = sim.diseases.meningitis # Shorten the name of the disease module
+            eligibility_conditions = meningitis.susceptible
+            
+            if self.age_range is not None: 
+                lower_age = self.age_range[0]
+                upper_age = self.age_range[1]
+                age_conditions = (sim.people.age >= lower_age) & (sim.people.age < upper_age)
+                eligibility_conditions = eligibility_conditions & age_conditions
+                
+            eligible_ids = sim.people.uid[eligibility_conditions] # Only susceptible people are eligible
+                
+            n_eligible = len(eligible_ids)  # Number of people who are eligible
+            to_vaccinate = self.prob > np.random.rand(n_eligible) # Define which of the n_eligible people get vaccinated
+            vaccine_ids = eligible_ids[to_vaccinate]
+            meningitis.immunity[vaccine_ids] += self.imm_boost
+            
 class Treatment(ss.Intervention):
     def __init__(self, timestep=10, prob=0.5, mean_dur_infection=5):
         super().__init__()
